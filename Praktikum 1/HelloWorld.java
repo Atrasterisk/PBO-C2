0ba760464449ad31387cc842988b20fbf13be22c
Farhan <106865383+Atrasterisk@users.noreply.github.com>
2024-02-21 09:04:14 +0700

    Add files via upload

---
+++ Praktikum 1/HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld
+//Catatan kalo mau dari cmd
+//Dari folder, klik kanan di dalam folder buat open di terminal
+// javac (Insert nama file).java buat compile
+// java (Insert nama file) buat run
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Hello, World!");
+	}
+}
