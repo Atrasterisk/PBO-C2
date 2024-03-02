203dffc21aa8b7d986aa339123dcb7f4c1cecb1b
Muhammad Farhan Hafiz Alkirami <106865383+Atrasterisk@users.noreply.github.com>
2024-03-02 16:44:45 +0700

    Add files via upload

---
+++ Praktikum 2/TugasModul/Soal 1/src/bangundatar/BujurSangkar.java
@@ -0,0 +1,23 @@
+package bangundatar;
+import poligon.Poligon;
+
+public class BujurSangkar extends Poligon
+{
+    private double panjangSisi;
+
+    public BujurSangkar(double panjangSisi)
+    {
+        this.jumlahSisi = 4;
+        this.panjangSisi = panjangSisi;
+    }
+
+    public double hitungLuas()
+    {
+        return panjangSisi * panjangSisi;
+    }
+
+    public double getPanjangSisi()
+    {
+        return this.panjangSisi;
+    }
+}
